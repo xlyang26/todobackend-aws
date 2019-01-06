8a66ac5e09e0927eff453c7ec6cad8ee156fdee9
lyang <lee.yang@charter.com>
2019-01-05 21:32:09 -0700

    added ch6_packer-ecs

---
+++ ch6_packer-ecs/scripts/storage.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+echo "### Configuring Docker Volumne Storage ###"
+sudo mkdir -p /data
+sudo mkfs.ext4 -L docker /dev/xvdcy
+echo -e "LABEL=docker\t/data\t\text4\tdefaults,noatime\t0\t0" | sudo tee -a /etc/fstab
+sudo mount -a
